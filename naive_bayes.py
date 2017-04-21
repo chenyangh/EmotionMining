1cd656563ebd6da641f5f73133dcde979c8bcf0e
chenyangh <hchenyang@hotmail.com>
2017-04-20 18:07:59 -0600

    make both balanced and imbalanced using scilearn package,

--- naive_bayes.py
+++ naive_bayes.py
@@ -6,11 +6,9 @@ import sklearn.metrics as sm
 import numpy as np
 from sklearn.linear_model import SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
-data_path = 'data'
-data_path1 = '3100DB'
 
-
-
+from measurements import CalculateFM
+from random import shuffle
 
 def meass(y_true, y_pred):
     m, n = y_pred.shape
@@ -32,9 +30,33 @@ class NaiveClassifier:
                 new_label.append(-1)
         return new_label
 
-    def fit(self, data, label):
-        new_label = self.gen_label(label)
-        self.model.fit(data, new_label)
+    def gen_balanced_label(self, label):
+        new_label = []
+        for item in label:
+            if self.num in item:
+                new_label.append(1)
+            else:
+                new_label.append(-1)
+        return new_label
+
+
+    def fit(self, data, label, is_balanced=False):
+        if not is_balanced:
+            new_label = self.gen_label(label)
+            self.model.fit(data, new_label)
+        else:
+            new_label = self.gen_label(label)
+            pos_idx = [idx for idx, d in enumerate(new_label) if d == 1]
+            neg_idx = [idx for idx, d in enumerate(new_label) if d != 1]
+            shuffle(neg_idx)
+            neg_idx = neg_idx[:len(pos_idx)]
+            new_label = np.array(new_label)
+            new_idx = np.concatenate((pos_idx, neg_idx))
+
+            data = data[new_idx, :]
+            new_label = new_label[new_idx]
+            self.model.fit(data, new_label)
+
 
     def predict(self, data,  __threshold):
         pred_result = self.model.predict(data)
@@ -48,12 +70,14 @@ class NaiveClassifier:
 
 def load_data():
     # Load the data
-    with open('train_test_ids_4k.pkl', 'br') as f:
+    data_path = 'data'
+
+    with open('others/train_test_ids_31k.pkl', 'br') as f:
         train_ids, test_ids = pickle.load(f)
     train_ids = set(train_ids)
     test_ids = set(test_ids)
     #
-    bow_file = os.path.join(data_path, 'Presence_Vector.csv')
+    bow_file = os.path.join(data_path, 'Presence_Vector_31k_train.csv')
     with open(bow_file, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter='\t')
         __train_data = []
@@ -64,7 +88,7 @@ def load_data():
             if key in train_ids:
                 __train_data.append(vector)
 
-    bow_file = os.path.join(data_path, 'Presence_Vector.csv')
+    bow_file = os.path.join(data_path, 'Presence_Vector_31k_test.csv')
     with open(bow_file, newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter='\t')
         __test_data = []
@@ -105,23 +129,32 @@ if __name__ == '__main__':
     threshold = 0.8
     num_classes = 9
 
-    if True:
-        print('loading data...')
+    print('loading data...')
+    if False:
         train_data, train_label, test_data, test_label = load_data()
 
-        with open('all_data', 'bw') as f:
+        with open('data/all_data', 'bw') as f:
             pickle.dump([train_data, train_label, test_data, test_label], f)
     else:
-        with open('all_data', 'br') as f:
+        with open('data/all_data', 'br') as f:
             train_data, train_label, test_data, test_label = pickle.load(f)
+    n = len(test_label)
+    print('Finish loading data...')
 
-    # Train model
-    # list_classifiers = []
-    # for i in range(num_classes):
-    #     new_classifier = NaiveClassifier(i)
-    #     print('Training', str(i), 'th classifier...')
-    #     new_classifier.fit(train_data, train_label)
-    #     list_classifiers.append(new_classifier)
+
+
+    # eng.edit('average_precision.m', nargout=0)
+    # eng.edit('F_measure.m', nargout=0)
+    # eng.edit('test.m', nargout=0)
+
+    train_data = np.array(train_data)
+    # ##Train model
+    list_classifiers = []
+    for i in range(num_classes):
+        new_classifier = NaiveClassifier(i)
+        print('Training', str(i), 'th classifier...')
+        new_classifier.fit(train_data, train_label, is_balanced=True)
+        list_classifiers.append(new_classifier)
 
     # with open('list_classifiers', 'bw') as f:
     #     pickle.dump(list_classifiers, f)
@@ -129,35 +162,37 @@ if __name__ == '__main__':
     # with open('list_classifiers', 'br') as f:
     #     list_classifiers = pickle.load(f)
 
-    # Test the classifiers
+    ## Test the classifiers
 
-    n = len(test_label)
-    # result = np.zeros([n, num_classes])
-    # for i in range(num_classes):
-    #     a_classifier = list_classifiers[i]
-    #     result[:, i] = a_classifier.predict(test_data, threshold)
-    # result = result.tolist()
+    result = np.zeros([n, num_classes])
+    for i in range(num_classes):
+        a_classifier = list_classifiers[i]
+        result[:, i] = a_classifier.predict(test_data, threshold)
+    result = result.tolist()
 
+    results = CalculateFM(test_label, result)
+
+    print('Results:', results)
     # with open('result', 'bw') as f:
     #     pickle.dump(result, f)
 
     # with open('result', 'br') as f:
     #     result = pickle.load(f)
     #
+
     # # magic start
-    from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
-    from sklearn.preprocessing import MultiLabelBinarizer
-    from sklearn.multioutput import MultiOutputClassifier
 
-    train_label_b = MultiLabelBinarizer().fit_transform(train_label)
-    clf = OneVsRestClassifier(RandomForestClassifier(n_estimators=20))
-    clf.fit(train_data, train_label_b)
-    result = clf.predict(test_data)
-
-    import matlab.engine
-    eng = matlab.engine.start_matlab()
-    print(eng.F_measure(result.tolist(), test_label, n, num_classes))
-
-    # eng.edit('average_precision.m', nargout=0)
-    # eng.edit('F_measure.m', nargout=0)
-    # eng.edit('test.m', nargout=0)
+    #  ##  One vs rest wrapper
+    # from sklearn.multiclass import OneVsOneClassifier, OneVsRestClassifier
+    # from sklearn.preprocessing import MultiLabelBinarizer
+    # from sklearn.multioutput import MultiOutputClassifier
+    #
+    # train_label_b = MultiLabelBinarizer().fit_transform(train_label)
+    # clf = OneVsRestClassifier(RandomForestClassifier(n_estimators=20))
+    # clf.fit(train_data, train_label_b)
+    # result = clf.predict(test_data)
+    #
+    # import matlab.engine
+    #
+    # eng = matlab.engine.start_matlab()
+    # print(eng.F_measure(result.tolist(), test_label, n, num_classes))
