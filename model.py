a64450b9fdae19812ff048321ad2951096f2a66b
Chenyang Huang <hchenyang@hotmail.com>
2017-12-18 23:23:41 -0700

    update

---
+++ model.py
@@ -0,0 +1,15 @@
+import torch
+import torch.nn as nn
+from data_helper import *
+
+
+class CNN_LSTM(nn.Module):
+    def __init__(self, n_words, emb_dim, target_dim,
+         ):
+        super(CNN_LSTM, self).__init__()
+        self.embedding = nn.Embedding(n_words, emb_dim)
+
+
+
+
+
